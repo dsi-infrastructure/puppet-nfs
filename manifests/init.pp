5e6bd4421bd34ff570082a3c0c905f60d73fe9a0
Léonard TAVAE <leonard.tavae@informatique.gov.pf>
2014-04-15 19:15:23 -1000

    Ajout de la distinction serveur et client.

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,12 +2,41 @@
 #
 #
 class nfs(
-    $client = hiera('nfs::client')) {
+    $client = hiera('nfs::client'),
+    $server = hiera('nfs::server', 'disable')) {
 
-    $packages = ['nfs-common', 'rpcbind']
+    if $client == 'enable' {
+        $packages = ['nfs-common', 'rpcbind']
+    }
+
+    if $server == 'enable' {
+       $packages = ['nfs-kernel-server']
+    }
 
     package { $packages:
         ensure => installed,
+    }
+
+    if $server == 'enable' {
+        file { '/etc/default/nfs-common':
+            ensure  => present,
+            owner   => 'root',
+            group   => 'root',
+            mode    => '0644',
+            source  => 'puppet:///modules/nfs/server-nfs-common',
+            #notify  => Exec['restart-nfs'],
+            require => Package['nfs-kernel-server'],
+        }
+
+        file { '/etc/default/nfs-kernel-server':
+            ensure  => present,
+            owner   => 'root',
+            group   => 'root',
+            mode    => '0644',
+            source  => 'puppet:///modules/nfs/server-nfs-kernel-server',
+            #notify  => Exec['restart-nfs'],
+            require => Package['nfs-kernel-server'],
+        }
     }
 
     if $client == 'enable' {
